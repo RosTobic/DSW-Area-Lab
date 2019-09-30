f6b3d99389a9c6c385bccef7e577e606e691b356
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-09-30 09:57:47 -0700

    added area

--- AreaLabUnfinished.py
+++ AreaLabUnfinished.py
@@ -1,26 +1,28 @@
-import math 
+import math #math library has code to do math
 import unittest
 
 def circleArea(radius): 
-    return 1
+    """return the area of a circle"""
+    return math.pi * radius * radius
 
 def rectArea(base, height):
-    return 1
+    """return the area of a rect"""
+    return base * height
 
 def trapArea(base1, base2, height):
-    return 1
+    """return the area of a trap"""
+    return  height * (base1 + base2) / 2
 
 def triArea(base,height):
-    return 1
-    
+    """return the area of a tri"""
+    return base * height / 2
 	
 class MyTest(unittest.TestCase):
     def testCircleArea(self):
-    
+        self.assertAlmostEqual(circleArea(5),25*math.pi)
     def testRectArea(self):
-    
+        self.assertAlmostEqual(rectArea(5,5),25)
     def testTrapArea(self):
-    
+        self.assertAlmostEqual(trapArea(5,5,5),25)
     def testTriArea(self):
-    
-    
+        self.assertAlmostEqual(triArea(5,5),12.5)
